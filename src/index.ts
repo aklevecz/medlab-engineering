8284a1dea090808b1bdeb1dc621a7b50a832da1e
traysi may <teh@raptor.pizza>
2019-09-07 00:02:05 -0600

    ports

--- src/index.ts
+++ src/index.ts
@@ -32,10 +32,9 @@ const ormFig =
         password: process.env.DB_PW,
         database: process.env.DB_NAME
       }
-    : null;
+    : undefined;
 
-console.log(ormFig);
-createConnection(null)
+createConnection(ormFig)
   .then(async connection => {
     // Create a new express application instance
     app = express();
@@ -47,8 +46,8 @@ createConnection(null)
 
     //Set all routes from routes folder
     app.use("/", routes);
-    app.listen(process.env.PORT || 4000, () => {
-      console.log("Server started on port 4000!");
+    app.listen(process.env.PORT || 3000, () => {
+      console.log("Server started on port 3000!");
     });
   })
   .catch(error => console.log(error));
